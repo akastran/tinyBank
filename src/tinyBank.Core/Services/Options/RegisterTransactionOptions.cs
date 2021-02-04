95fbe5e097f9ecc7e0089243950aad80b2194212
ΑΝΔΡΕΑΣ ΚΑΣΤΡΙΣΙΟΣ <KASTRISIOS.A@nbg.gr>
2021-02-04 11:25:19 +0200

    TransactionService created

---
+++ src/tinyBank.Core/Services/Options/RegisterTransactionOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tinyBank.Core.Services.Options
+{
+    public class RegisterTransactionOptions
+    {
+        public string AccountId { get; set; }
+        public DateTimeOffset TransactionCreated { get; set; }
+        public decimal TransactionAmount { get; set; }
+    }
+}
---
+++ src/tinyBank.Core/Services/TransactionService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tinyBank.Core.Data;
+using tinyBank.Core.Model;
+using tinyBank.Core.Services.Options;
+
+namespace tinyBank.Core.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private BankDbContext _dbContext;
+
+        public TransactionService(BankDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Transaction Register(RegisterTransactionOptions options)
+        {
+            if (string.IsNullOrWhiteSpace(options?.AccountId))
+            {
+                return null;
+            }
+
+            if (options?.TransactionAmount == 0)
+            {
+                return null;
+            }
+
+            var transaction = new Transaction()
+            {
+                AccountId = options.AccountId,
+                TransactionCreated = DateTimeOffset.Now,
+                TransactionAmount = options.TransactionAmount
+            };
+
+            _dbContext.Add(transaction);
+            _dbContext.SaveChanges();
+
+            return transaction;
+        }
+    }
+}
